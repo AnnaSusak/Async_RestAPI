d24e92775d3715f8eb9edc0a86981b52e082079d
AnnaSusak <anna.i.susak@gmail.com>
2023-02-08 22:02:45 +0300

    lesson2_comics

---
+++ RestApi_Library/Models/ModelComic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApi_Library.Models
+{
+    public class ModelComic
+    {
+        public int Num { get; set; }
+        public string Img { get; set; }
+    }
+}
